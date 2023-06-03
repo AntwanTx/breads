6c290816a2b98137aefe6486616497193941bf19
Anthony Munoz <amunoz021@gmail.com>
2023-06-03 13:04:07 -0500

    Added CSS and 404

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
 //Dependencies
 const express = require ('express')
+const app = express()
 
 //Configuration
 require('dotenv').config()
 const PORT = process.env.PORT
-const app = express()
 console.log(PORT)
-
-//Bread
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
-
-//Middleware
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
 
 //Routes
 app.get('/',(req,res)=>{
     res.send('Welcome to my Bread World')
 })
 
+//Middleware
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+app.use(express.static('public'))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+ 
 //Listen
 app.listen(PORT,()=>{
-    console.log('listening on port',PORT);
+    console.log('listening on port',PORT)
 })
